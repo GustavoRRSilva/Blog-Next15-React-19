91bad1a9f4ecd52f2f91453d7cbd1585b6d8f224
Gustavo <gstvrrds@gmail.com>
2025-05-17 10:42:57 -0300

    feat:adding json-repository and post-repository

--- src/repositories/post/json-repository.ts
+++ src/repositories/post/json-repository.ts
@@ -1,16 +1,53 @@
 import { PostModel } from '@/models/post/post-model';
 import { PostRepository } from './post-repository';
+import { resolve } from 'node:path';
+import { readFile } from 'node:fs/promises';
 
 const ROOT_DIR = process.cwd();
-console.log(ROOT_DIR);
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  'src',
+  'db',
+  'seed',
+  'posts.json',
+);
+console.log(JSON_POSTS_FILE_PATH);
 //Implemento o PostRepository, para implementar as opções com as tipagens passadas anteriormente
-export class JsonRepository
-  implements PostRepository
-{
-  private async readFromDisk() {}
-  findAll(): Promise<PostModel[]> {}
+export class JsonRepository implements PostRepository {
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
+
+    const parsedJson = JSON.parse(jsonContent);
+
+    const { posts } = parsedJson;
+    return posts;
+  }
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const allPosts = await this.findAll();
+    const post = allPosts.find(post => post.id === id);
+    if (!post) throw new Error('Post não encontrado');
+    return post;
+  }
 }
 
-//Cria o tipo para ser exportado futuramente
-export const postRepository =
-  new JsonRepository();
+//Cria o tipo para ser exportado futuramente e coloca o tipo PostRepository para somente mostras as coisas da interface NÃO da implementação
+export const postRepository: PostRepository = new JsonRepository();
+
+/* (async () => {
+  const posts =
+    await postRepository.findAll();
+  posts.forEach(post =>
+    console.log(post.author),
+  );
+})(); */
+
+/* (async () => {
+  const findById =
+    await postRepository.findById('');
+  console.log(findById);
+})(); */
--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -3,4 +3,7 @@ import { PostModel } from '@/models/post/post-model';
 //Aqui o ele cria basicamente o tipo, para futuramente ser utilizado em outro repository(json, sqlLite, postgrees e etc )
 export interface PostRepository {
   findAll(): Promise<PostModel[]>;
+  findById(
+    id: string,
+  ): Promise<PostModel>;
 }
